d1856c413b7600b1a77b2f9fb14d1277907aa2a6
denriq55 <142200008+denriq55@users.noreply.github.com>
2025-07-09 20:37:55 -0700

    add methods to display status, change time of day and frequency, and display summary

--- oopworld.php
+++ oopworld.php
@@ -8,13 +8,42 @@ class Habit {
     public $isComplete;
 
     //Set up habit data
-    public function __construct($habitName, $category, $timeOfDay, $frequency) {
+    public function __construct($habitName, $category, $frequency, $timeOfDay) {
         $this->habitName = $habitName;
         $this->category = $category;
-        $this->timeOfDay = $timeOfDay;
         $this->frequency = $frequency;
+        $this->timeOfDay = $timeOfDay;
         $this->isComplete = false; 
     }
 
+    //Method to display status
+    public function displayStatus() {
+        if (!$this->isComplete) {
+            return "❌";
+        } else {
+            return "✅";
+        }
+    }
+
+    //Method to change time of day. 
+    //Possible values: any, morning, afternoon, night
+    public function updateTimeOfDay($newTime) {
+        $this->timeOfDay = $newTime;
+        return "Time of day updated to: " . $this->timeOfDay;
+    }
+
+    //Method to change frequency. 
+    public function updateFrequency($newFrequency) {
+        $this->frequency = $newFrequency;
+        return "Frequency updated to: " . $this->frequency;
+    }
+
+    //Method to display summary of habit.
+    public function displaySummary() {
+        $status = $this->displayStatus();
+        return $this->habitName . "|" . $this->category . "|" . $this->frequency . "|" . $this->timeOfDay .  "|" . $status;
+    }
 }
+
+
 ?>
